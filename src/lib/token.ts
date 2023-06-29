7ad202d4b2c0f4e51a9e77e41800655a37689148
Justin Xiao <o927416847@gmail.com>
2023-06-29 19:50:25 +0800

    feat: added utils

---
+++ src/lib/token.ts
@@ -0,0 +1,29 @@
+import { SignJWT, jwtVerify } from "jose";
+
+export const signJWT = async (payload: { sub: string }, options: { exp: string }) => {
+    try {
+        const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY);
+        const alg = "HS256";
+        return new SignJWT(payload)
+            .setProtectedHeader({ alg })
+            .setExpirationTime(options.exp)
+            .setIssuedAt()
+            .setSubject(payload.sub)
+            .sign(secret);
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const verifyJWT = async <T>(token: string): Promise<T> => {
+    try {
+        const { payload } = await jwtVerify(
+            token,
+            new TextEncoder().encode(process.env.JWT_SECRET_KEY)
+        );
+        return payload as T;
+    } catch (error) {
+        console.log(error);
+        throw new Error("Your token has expired.");
+    }
+};
