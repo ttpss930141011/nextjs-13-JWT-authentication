80b57d1e08f0d1eb6b359653a640aa6a743518bc
Justin Xiao <o927416847@gmail.com>
2023-06-30 21:49:02 +0800

    fix: FOUC problem

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
+import RootStyleRegistry from "./emotion";
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
     return (
-        <MantineProvider withGlobalStyles withNormalizeCSS>
-            {children}
-            <Notifications />
-        </MantineProvider>
+        <RootStyleRegistry>
+            <MantineProvider>
+                {children}
+                <Notifications />
+            </MantineProvider>
+        </RootStyleRegistry>
     );
 }
